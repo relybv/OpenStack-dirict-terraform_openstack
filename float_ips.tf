563715d781b7d52b1676ec92530d78296826257f
Patrick Mans <p.mans@rely.nl>
2016-08-03 14:20:15 +0200

    Changed various configs

--- float_ips.tf
+++ float_ips.tf
@@ -6,7 +6,7 @@ resource "openstack_compute_floatingip_v2" "lb" {
 }
 
 resource "openstack_compute_floatingip_v2" "jump" {
-  depends_on = ["openstack_networking_router_interface_v2.jump"]
+  depends_on = ["openstack_networking_router_interface_v2.frontend"]
   region = "${var.region}"
   pool = "${var.pool}"
 }
